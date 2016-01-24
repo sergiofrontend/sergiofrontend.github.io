4ed27863e2f0bbae8a094a62ab545b3324aad178
Sergio Antonio <sergio.mantonio@gmail.com>
2016-01-23 20:27:57 -0800

    add ngRoute

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('sergioPortfolioApp', ['ui.bootstrap', 'bootstrapLightbox', 'ngAnimate', 'angular-scroll-animate']);
+var app = angular.module('sergioPortfolioApp', ['ui.bootstrap', 'bootstrapLightbox', 'ngAnimate', 'angular-scroll-animate', 'ngRoute']);
+
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode({ enabled: true, requireBase: true });	
+});
